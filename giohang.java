293dfcc0beebb7133a9591a454ed95f076269f3a
nguyentuan2k7 <25070424@vnu.edu.vn>
2026-04-23 03:49:10 +0700

    .

---
+++ giohang.java
@@ -0,0 +1,14 @@
+// voucher.js
+
+const VOUCHERS = [
+  {
+    code: "SALE10",
+    type: "percent", // giảm %
+    value: 10
+  },
+  {
+    code: "GIAM50K",
+    type: "fixed", // giảm tiền
+    value: 50000
+  }
+];
