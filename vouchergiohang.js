b4cbdbe971f2615c8a3e17341811130401c0be57
nguyentuan2k7 <25070424@vnu.edu.vn>
2026-04-24 11:33:45 +0700

    .

--- vouchergiohang.js
+++ vouchergiohang.js
@@ -2,7 +2,8 @@ const DANH_SACH_VOUCHER = [
     { code: "GIAM10", type: "percent", value: 10 },
     { code: "GIAM50K", type: "fixed", value: 50000 },
     { code: "SALE30", type: "percent", value: 30 },
-    { code: "TIKPAS2026", type: "percent", value: 20 }
+    { code: "TIKPAS2026", type: "percent", value: 20 },
+    {code: "SALESAPSAN", type: "percent", value: 50 }
 ];
 
 // Xuất ra để HTML dùng
